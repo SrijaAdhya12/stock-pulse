16a37cc3feed44bd8f5fc28d14d0364e8c30c4db
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2025-07-05 03:27:25 +0530

    feat: added project setup

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,27 @@
+// src/components/Button.jsx
+import { useState } from 'react'
+import axios from 'axios'
+
+const Button = () => {
+	const [response, setResponse] = useState('')
+	const VITE_SERVER_URL = import.meta.env.VITE_SERVER_URL
+
+	const handleClick = async () => {
+		try {
+			const res = await axios.get(`${VITE_SERVER_URL}/health`)
+			setResponse(res.data)
+		} catch (error) {
+			console.error('Error fetching health:', error)
+			setResponse('Error fetching health status')
+		}
+	}
+
+	return (
+		<div>
+			<button onClick={handleClick}>Check Server Health</button>
+			{response && <p>{response}</p>}
+		</div>
+	)
+}
+
+export default Button
